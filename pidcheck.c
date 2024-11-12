199b2e9f3b04816a7966c3d4630293df531dc7a3
A.R.Basil <144218037+arbasil05@users.noreply.github.com>
2024-11-12 07:54:03 +0530

    Update pidcheck.c

--- pidcheck.c
+++ pidcheck.c
@@ -1,1 +1,17 @@
-C Program to print process ID and parent Process ID using Linux API system calls
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main() {
+    int pid;
+    pid = fork();
+    if (pid == 0) {
+        printf("Iam Child my pid is %d\n", getpid());
+        printf("My parent pid is %d\n", getppid());
+        exit(0);
+    } else {
+        printf("Iam parent my pid is %d\n", getpid());
+        sleep(100);
+        exit(0);
+    }
+}
